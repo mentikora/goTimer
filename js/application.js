d5dd276fdfae608846a9e470a6d5d4e2e5fbd312
Mentikora <notender_boy@ukr.net>
2016-05-28 03:09:10 +0300

    dev

--- js/application.js
+++ js/application.js
@@ -4,4 +4,27 @@ $('document').ready(function(){
 		$(this).toggleClass('vertical');
 	});
 
+	$('.next').on('click', function(){
+		$('html,body').animate({
+	        scrollTop: $(this).closest('.step').next().offset().top
+	    }, 1000);
+	});
+
+	$('.prev').on('click', function(){
+		$('html,body').animate({
+	        scrollTop: $(this).closest('.step').prev().offset().top
+	    }, 1000);
+	});
+
+	$('.start').on('click', function(){
+		$('.player').css('padding','5%').addClass('ingame');
+		$(this).closest('.step').find('input').addClass('hidden');
+		$(this).closest('.player').addClass('active');
+	});
+
+	$('.ingame').on('click', function(){
+		$(this).siblings('.ingame').addClass('active');
+		$(this).removeClass('active');
+	});
+
 });
